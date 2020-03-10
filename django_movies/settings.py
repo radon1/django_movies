84a3f7619d51f6033a226bca8e064a8319a0506e
radon1 <42577596+radon1@users.noreply.github.com>
2020-03-10 21:49:50 +0300

    commit

--- django_movies/settings.py
+++ django_movies/settings.py
@@ -102,6 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -120,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
