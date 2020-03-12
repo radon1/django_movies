46bc03a7327a1b14120c9758f5b19780940b013d
radon1 <42577596+radon1@users.noreply.github.com>
2020-03-12 13:52:50 +0300

    commit

--- movies/views.py
+++ movies/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 from .models import Movie
 
 
-class MoviesView(View):
+class MoviesView(ListView):
     '''Список фильмов'''
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movies.html", {'movie_list': movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    # template_name = 'movies/movie_list.html'
+
+
+class MovieDetailView(DetailView):
+    '''Полное описание фильма'''
+    model = Movie
+    slug_field = 'url'
